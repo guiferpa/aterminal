82310d7bf47ed942c4944d5fd2b3399ea2ff6741
Guilherme Paixão <guiferpa@gmail.com>
2024-07-08 10:00:13 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 git clone https://github.com/guiferpa/aterminal.git aterminal
-rm -rf $ZSH/plugins/aterminal
-mv aterminal $ZSH/plugins/aterminal
+rm -rf $ZSH/themes/aterminal
+mv aterminal $ZSH/themes/aterminal
